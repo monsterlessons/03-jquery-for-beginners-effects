260aca3054fcd129110115c307fd0bca81b31fb1
Alexander Kochergin <ejiqpep@gmail.com>
2017-06-03 20:56:51 +0200

    03 lesson

--- code.js
+++ code.js
@@ -1,22 +1,39 @@
-$('.submit').on('click', function () {
-  console.log('clicked')
-})
-
-var $square = $('.square')
-var enterHandler = function () {
-  console.log('square enter')
-  $square.addClass('active')
+var $fadeOutButton = $('.fadeout')
+var $fadeInButton = $('.fadein')
+var $text = $('.text')
+var $text2 = $('.text2')
+var fadeOut = function (element, ms) {
+  var opacity = 1
+  var timer = setInterval(function () {
+    if (opacity <= 0.1) {
+      clearInterval(timer)
+      element.style.display = 'none'
+    }
+    element.style.opacity = opacity
+    opacity -= 0.1
+  }, ms/10)
 }
-var leaveHandler = function () {
-  console.log('square leve')
-  $square.removeClass('active')
+var fadeIn = function (element, ms) {
+  var opacity = 0.1
+  element.style.display = 'block'
+  element.style.opacity = opacity
+  var timer = setInterval(function () {
+    if (opacity >= 1) {
+      clearInterval(timer)
+    }
+    element.style.opacity = opacity
+    opacity += 0.1
+  }, ms/10)
+}
+var fadeOutHandler = function () {
+  // $text.fadeOut(3000)
+  fadeOut($text[0], 3000)
 }
 
-$square.mouseenter(enterHandler)
-  .mouseleave(leaveHandler)
+var fadeInHandler = function () {
+  // $text2.fadeIn(3000)
+  fadeIn($text2[0], 3000)
+}
 
-$('body').on('click', '.byJavascript', function () {
-  console.log('clicked on js element')
-})
-
-$('body').append('<div class="byJavascript">Created by javascript</div>')
+$fadeOutButton.click(fadeOutHandler)
+$fadeInButton.click(fadeInHandler)
